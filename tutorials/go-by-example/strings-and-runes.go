198cc114fbef8e14f0b9f318fc359c93efb9596d
Abhinav Mishra <abhinav.thinktank@gmail.com>
2022-03-06 01:30:20 -0500

    Added unfinished work

---
+++ tutorials/go-by-example/strings-and-runes.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+	Go string is a read only slice of bytes
+	The language and standard library treat strings specially
+	containers of text encoded in UTF-8.
+	Unlike other languages where strings are made of "characters".
+	In GO - a character concept is called a "rune", which is an integer
+	representing unicode code point
+*/
+
+func main() {
+
+	const s = "สวัสดี"
+
+}
